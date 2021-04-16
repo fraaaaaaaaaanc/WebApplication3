d6cc28d8afcf74a995404d6d99bb163df4046d85
danila francev <frantsev_94@mail.ru>
2021-04-16 21:47:14 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult glav()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult poisk()
+        {
+            return View();
+        }
+        public IActionResult Poisk2()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
